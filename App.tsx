f2f9f6af97ff3c27259bc96ab6804dd1d979e1a3
hamza amin <hamzaamin.skylinxtech@gmail.com>
2024-10-01 16:40:21 +0500

    pushing recipe app on github repository

--- App.tsx
+++ App.tsx
@@ -6,113 +6,47 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet} from 'react-native';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import CategoryScreen from './screens/CategoryScreen';
+import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+import MealsOverView from './screens/MealsOverview';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MealDetailScreen from './screens/MealDetailScreen';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#353401',
+            },
+            headerTintColor: 'white',
+            contentStyle: {backgroundColor: '#3f2f25'},
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          <Stack.Screen
+            name="CategoryScreen"
+            component={CategoryScreen}
+            options={{
+              title: 'All Category',
+            }}
+          />
+          <Stack.Screen name="MealsOverView" component={MealsOverView} />
+          <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} />
+          {/* return <CategoryScreen />; */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
+  // <CategoryScreen />;
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
--- android/app/src/main/java/com/rncoursenew/MainActivity.kt
+++ android/app/src/main/java/com/rncoursenew/MainActivity.kt
@@ -1,4 +1,7 @@
+
 package com.rncoursenew
+import android.os.Bundle;
+
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,8 +14,11 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "rncoursenew"
+   override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
 
+  override fun getMainComponentName(): String = "rncoursenew"
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
